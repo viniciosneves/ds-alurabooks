1dc4d057b02b3904f240ca6f18cff007bf2accea
Vinicios Neves <vinny@macbook.lan>
2022-09-02 15:31:16 +0100

    adicionando darkmonde ao CampoTexto

--- src/componentes/AbCampoTexto/index.tsx
+++ src/componentes/AbCampoTexto/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 
-const LabelEstilizada = styled.label`
-    color: #002F52;
+const LabelEstilizada = styled.label<{ darkmode: boolean }>`
+    color: ${(props) => props.darkmode ? '#FFF' : '#002F52'};
     display: block;
     font-weight: 700;
     font-size: 16px;
@@ -11,21 +11,32 @@ const LabelEstilizada = styled.label`
     margin-bottom: 8px
 `
 
-const InputEstilizado = styled.input`
+interface InputEstilizadoProps {
+    placeholderAlign: 'left' | 'center' | 'right';
+    darkmode: boolean
+}
+
+const InputEstilizado = styled.input<InputEstilizadoProps>`
     font-size: 16px;
     line-height: 24px;
-    color: #002F52;
     padding: 8px 24px;
-    border: 1px solid #002F52;
+    border: 1px solid;
     border-radius: 45px;
     &:focus{
         outline: none;
     }
     width: 100%;
     box-sizing: border-box;
+    background: ${(props: InputEstilizadoProps) => props.darkmode ? 'transparent' : '#FFF'};
+    border-color: ${(props: InputEstilizadoProps) => props.darkmode ? '#FFF' : '#002F52'};
+    text-align: ${(props: InputEstilizadoProps) => props.placeholderAlign};
+    ::placeholder,
+    ::-webkit-input-placeholder  {
+        color: ${(props: InputEstilizadoProps) => props.darkmode ? '#FFF' : '#002F52'};
+    }
 `
 
-export interface AbCampoTextoProps {
+export interface AbCampoTextoProps extends InputEstilizadoProps {
     label?: string;
     placeholder?: string;
     value: string;
@@ -33,17 +44,27 @@ export interface AbCampoTextoProps {
     onChange: (value: string) => void
 }
 
-export const AbCampoTexto = ({ label, value, onChange, type = 'text', placeholder = "" }: AbCampoTextoProps) => {
+export const AbCampoTexto = ({ 
+        label, 
+        value, 
+        onChange, 
+        type = 'text', 
+        placeholder = "", 
+        placeholderAlign = 'left', 
+        darkmode = false 
+}: AbCampoTextoProps) => {
     return (
         <div>
-            {label && <LabelEstilizada>
+            {label && <LabelEstilizada darkmode={darkmode}>
                 {label}
             </LabelEstilizada>}
             <InputEstilizado
                 placeholder={placeholder}
+                placeholderAlign={placeholderAlign}
+                darkmode={darkmode}
                 type={type}
                 value={value}
-                onChange={event => onChange(event.target.value)} 
+                onChange={event => onChange(event.target.value)}
             />
         </div>
     )
