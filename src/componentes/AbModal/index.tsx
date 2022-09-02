21228989ad42bb252d0e076a05b3d2c1ae5fa75d
Vinicios Neves <vinny@macbook.lan>
2022-09-02 17:43:28 +0100

    adicionando AbModal

---
+++ src/componentes/AbModal/index.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react"
+import styled from "styled-components";
+
+export interface AbModalProps {
+    children: ReactNode,
+    aberta: boolean,
+    aoFechar: () => void
+}
+
+const JanelaModal = styled.div`
+    position: absolute;
+    padding: 64px;
+    background: #FFFFFF;
+    box-shadow: 4px 4px 24px -4px rgba(0, 0, 0, 0.15);
+    border-radius: 24px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`
+const FundoModal = styled.div`
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    background: rgba(101, 101, 101, 0.85);
+`
+
+export const AbModal = ({ children, aberta, aoFechar }: AbModalProps) => {
+    if (!aberta) {
+        return <></>
+    }
+    return (
+        <>
+            <FundoModal onClick={aoFechar} />
+            <JanelaModal>
+                {children}
+            </JanelaModal>
+        </>
+    )
+}
---
+++ stories/AbModal.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AbModal, AbModalProps } from '../src'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+    title: 'Componentes/AbModal',
+    component: AbModal
+} as ComponentMeta<typeof AbModal>
+
+const Template: ComponentStory<typeof AbModal> = (args) => <AbModal {...args}/>
+
+export const Primario = Template.bind({})
+
+Primario.args = {
+    aberta: true,
+    children: <h1>Uma modal bacanuda</h1>
+} as AbModalProps
