036ddd605d111d5b1b829639647a797210d26d59
Vinicios Neves <vinny@macbook.lan>
2022-11-03 20:51:21 +0000

    atualizando componente AbTag

--- src/componentes/AbTag/index.tsx
+++ src/componentes/AbTag/index.tsx
@@ -1,23 +1,26 @@
 import React from "react"
 import styled from "styled-components";
 
-export interface AbTagProps {
+export interface AbTagStyleProps {
+    contexto?: 'primario' | 'secundario'
+}
+export interface AbTagProps extends AbTagStyleProps {
     texto: string
 }
 
-const TagStyled = styled.div`
-    padding: 24px 32px;
+const TagStyled = styled.div<AbTagStyleProps>`
+    padding: 16px 24px;
     color: #FFFFFF;
-    background: #EB9B00;
+    background: ${(props: AbTagStyleProps) => props.contexto === 'primario' ? '#EB9B00' : 'linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)'};
     font-weight: 700;
     font-size: 24px;
     display: inline-block;
     font-family: sans-serif;
 `
 
-export const AbTag = ({ texto }: AbTagProps) => {
+export const AbTag = ({ texto, contexto = 'primario' }: AbTagProps) => {
     return (
-        <TagStyled>
+        <TagStyled contexto={contexto}>
             {texto}
         </TagStyled>
     )
